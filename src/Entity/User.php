346aae1dda4d18604a063e1c068b7b38467cd78a
Hamza <noualihamza@hotmail.com>
2022-05-24 12:39:02 +0200

    add Registration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -53,6 +53,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'datetime_immutable', options: ['DEFAULT' => 'CURRENT_TIMESTAMP'])]
     private $createdat;
 
+    #[ORM\Column(type: 'boolean')]
+    private $is_verified = false;
+
+
     public function __construct()
     {
         $this->createdat = new \DateTimeImmutable();
@@ -232,6 +236,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setCreatedat(\DateTimeImmutable $createdat): self
     {
         $this->createdat = $createdat;
+
+        return $this;
+    }
+
+    public function getIsVerified(): ?bool
+    {
+        return $this->is_verified;
+
+    }
+
+    public function setIsVerified(bool $is_verified): self
+    {
+        $this->is_verified = $is_verified;
 
         return $this;
     }
