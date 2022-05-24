d5a7045fac093853169ded53556460338af6810f
Hamza <noualihamza@hotmail.com>
2022-05-24 11:21:24 +0200

    add Registration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -26,6 +26,38 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string')]
     private $password;
 
+    #[ORM\Column(type: 'string', length: 45)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 45)]
+    private $firstname;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private $phonenumber;
+
+    #[ORM\Column(type: 'string', length: 45)]
+    private $country;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private $zipcode;
+
+    #[ORM\Column(type: 'string', length: 45)]
+    private $city;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $address;
+
+    #[ORM\Column(type: 'date_immutable')]
+    private $dateofbirth;
+
+    #[ORM\Column(type: 'datetime_immutable', options: ['DEFAULT' => 'CURRENT_TIMESTAMP'])]
+    private $createdat;
+
+    public function __construct()
+    {
+        $this->createdat = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -95,4 +127,113 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getPhonenumber(): ?string
+    {
+        return $this->phonenumber;
+    }
+
+    public function setPhonenumber(string $phonenumber): self
+    {
+        $this->phonenumber = $phonenumber;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getZipcode(): ?string
+    {
+        return $this->zipcode;
+    }
+
+    public function setZipcode(string $zipcode): self
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getDateofbirth(): ?\DateTimeImmutable
+    {
+        return $this->dateofbirth;
+    }
+
+    public function setDateofbirth(\DateTimeImmutable $dateofbirth): self
+    {
+        $this->dateofbirth = $dateofbirth;
+
+        return $this;
+    }
+
+    public function getCreatedat(): ?\DateTimeImmutable
+    {
+        return $this->createdat;
+    }
+
+    public function setCreatedat(\DateTimeImmutable $createdat): self
+    {
+        $this->createdat = $createdat;
+
+        return $this;
+    }
+
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,11 +3,16 @@
 namespace App\Form;
 
 use App\Entity\User;
+
+use DateTimeImmutable;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,14 +22,23 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+            ->add('email', EmailType::class, ['label' => 'E-mail'])
+            ->add('name', TextType::class, ['label' =>'Nom'])
+            ->add('firstname', TextType::class, ['label' =>'Prénom'])
+            ->add('phonenumber', TextType::class, ['label' =>'Numéro'])
+            ->add('country', TextType::class, ['label' =>'Pays'])
+            ->add('zipcode', TextType::class, ['label' =>'Code Postal'])
+            ->add('city', TextType::class, ['label' =>'Ville'])
+            ->add('address', TextType::class, ['label' =>'Adresse'])
+            ->add('dateofbirth')
+            ->add('RgpdConsent', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'label' => 'En m\'inscrvant à ce site j\'accepte les condissions d\'utilisation'
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
