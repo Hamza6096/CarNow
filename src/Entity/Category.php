a2da1464978ac7a5c275fedaf31a9c2a30debf6d
Hamza <noualihamza@hotmail.com>
2022-05-25 16:02:52 +0200

    add crud category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -21,9 +21,19 @@ class Category
     #[ORM\Column(type: 'string', length: 45)]
     private $nameCategory;
 
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $slug;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'categories')]
+    private $parent;
+
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+    private $categories;
+
     public function __construct()
     {
         $this->car = new ArrayCollection();
+        $this->categories = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -76,5 +86,59 @@ class Category
     public function __toString()
     {
         return $this->nameCategory;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(self $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+            $category->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(self $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            // set the owning side to null (unless already changed)
+            if ($category->getParent() === $this) {
+                $category->setParent(null);
+            }
+        }
+
+        return $this;
     }
 }
